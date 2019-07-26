9910b7692145a987e46c55981ffbf9bf659d3dd5
gisempire <gisempire@foxmail.com>
2019-07-26 19:51:36 +0800

    Update 1.1.py

--- 1.1.py
+++ 1.1.py
@@ -7,16 +7,11 @@ y_data = np.dot([0.1,0.2],x_data)+0.3
 print(x_data)
 print(y_data)
 
-# 构造一个线性模型
-# 
+# 构造一个线性模型 
 b = tf.Variable(tf.zeros([1]))
 W = tf.Variable(tf.random_uniform([1, 2], -1.0, 1.0))
 y = tf.matmul(W, x_data) + b
 
-# 最小化方差
-loss = tf.reduce_mean(tf.square(y - y_data))
-optimizer = tf.train.GradientDescentOptimizer(0.5)
-train = optimizer.minimize(loss)
 
 # 初始化变量
 #将init = tf.initialize_all_variables() # old api 改为
@@ -26,8 +21,23 @@ init = tf.global_variables_initializer() #new api
 sess = tf.Session()
 sess.run(init)
 
+print(sess.run(b))
+print(sess.run(W))
+print(sess.run(y))
+print('--')
+print(sess.run(y - y_data))
+print(sess.run(tf.square(y - y_data)))
+print('--')
+# 最小化方差
+loss = tf.reduce_mean(tf.square(y - y_data))
+print(sess.run(loss))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+
+
 # 拟合平面
-for step in range(0, 21):
+for step in range(0, 1000):
     sess.run(train)
-    if step % 20 == 0:
-        print(step, sess.run(W), sess.run(b))
+    if step % 10 == 0:
+        print(step, sess.run(W), sess.run(b),sess.run(loss))
