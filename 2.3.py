033cd4e92bdb0180f22f258f4972fe691a1c795e
gisempire <gisempire@foxmail.com>
2019-08-20 17:27:09 +0800

    pro

--- 2.3.py
+++ 2.3.py
@@ -11,3 +11,31 @@ b=tf.Variable(tf.zeros([10]))
 
 y=tf.nn.softmax(tf.matmul(x,w)+b)
 
+y_=tf.placeholder(tf.float32,[None,10])
+
+cross_=-tf.reduce_sum(y_*tf.log(y))
+
+train_step=tf.train.GradientDescentOptimizer(0.01).minimize(cross_)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for i in range(1000):
+      batch_xs, batch_ys = mnist.train.next_batch(100)
+      sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+      if i%100==0:
+          print("======="+str(i)+"=======")
+          correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+          accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+          ys=sess.run(accuracy,feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+          print(ys)
+
+
+
+          
+                              
+
+ 
