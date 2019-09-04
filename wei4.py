75a299119fbaa91b7e3928b065795ff31b8f9e28
gisempire <gisempire@foxmail.com>
2019-09-04 16:29:05 +0800

    Update wei4.py

--- wei4.py
+++ wei4.py
@@ -2,6 +2,27 @@
 #导入官方cifar10模块
 import cifar10
 import tensorflow as tf
+import os
+
+
+#检测CIFAR-10数据是否存在，如果不存在则返回False
+def check_cifar10_data_files(filenames):
+    for file in filenames:
+        if os.path.exists(file) == False:
+            print('Not found cifar10 data.')
+            return False
+    return True
+ 
+#获取图片前的预处理，检测CIFAR10数据是否存在，如果不存在直接退出
+#如果存在，用string_input_producer函数创建文件名队列，
+# 并且通过get_record函数获取图片标签和图片数据，并返回
+def get_image(data_path):
+    filenames = [os.path.join(data_path, "data_batch_%d.bin" % i) for i in range(1, 6)]
+    print(filenames)
+    if check_cifar10_data_files(filenames) == False:
+        exit()
+    queue = tf.train.string_input_producer(filenames)
+    return get_record(queue)
  
 #tf.app.flags.FLAGS是tensorflow的一个内部全局变量存储器
 FLAGS = tf.app.flags.FLAGS
@@ -13,9 +34,11 @@ FLAGS.data_dir = './cifar10_data'
  
 #如果不存在数据文件则下载，并且解压
 cifar10.maybe_download_and_extract()
-    
-    
 
+#将获取的图片保存到这里
+image_save_path = './cifar10_image/'
+if os.path.exists(image_save_path) == False:
+    os.mkdir(image_save_path)
 
 
           
