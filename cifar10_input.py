8e7fc809ea59cb60e3bf86d74bf57d1fb3d2f3e1
gisempire <gisempire@foxmail.com>
2019-09-05 11:23:03 +0800

    .

--- cifar10_input.py
+++ cifar10_input.py
@@ -148,7 +148,7 @@ def distorted_inputs(data_dir, batch_size):
     images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
     labels: Labels. 1D tensor of [batch_size] size.
   """
-  filenames = [os.path.join(data_dir, 'data_batch_%d.bin' % i) for i in xrange(1, 3)]
+  filenames = [os.path.join(data_dir, 'data_batch_%d.bin' % i) for i in xrange(1, 6)]
 
   print(filenames)
   
